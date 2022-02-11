c7ddb46169b3e36b80d7031a131d39a3ced6c86f
Your Name <you@example.com>
2022-02-11 16:18:20 +0800

    first commit

---
+++ android/build/generated/source/buildConfig/release/com/rnim/rn/audio/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rnim.rn.audio;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.rnim.rn.audio";
+  public static final String BUILD_TYPE = "release";
+}
